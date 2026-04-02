0e4c1556085c142dbd65d7812a1291b8f8e00785
Atish Sinha <atish21sinha@gmail.com>
2026-04-03 00:04:07 +0530

    footerAdded

--- 04gits/35gits/first.js
+++ 04gits/35gits/first.js
@@ -2,11 +2,11 @@ const button ="Added a button";
 
 
 
-console.log(button)
+console.log(button);
 const login="login page added";
 console.log(login);
 
 const upi="Integrated the upi";
-console.log(upi)
-const footer="footer added in our website"
-console.log(footer)
+console.log(upi);
+const footer="footer added in our website";
+console.log(footer);
